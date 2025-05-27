26395ac8530f8466796725bfab811e3698867e6b
Nimue <s.n.ruschell@gmail.com>
2025-05-27 16:46:29 -0400

    added functionality and styling

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+//TO-DO: display an artwork on loading without button press
 const header = document.querySelector("h1");
 
 fetch(
-  "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number"
+  "https://api.artic.edu/api/v1/artworks?fields=id,image_id,title,artist_title,date_display,provenance_text,is_public_domain&limit=40"
 )
   .then((res) => {
     if (!res.ok) {
@@ -9,4 +10,31 @@ fetch(
     }
     return res.json();
   })
-  .then((data) => console.log(data));
+  .then((data) => {
+    const collection = data.data; //target artwork object and store in collection
+    console.log(collection);
+
+    function refresh() {
+      let i = Math.floor(Math.random() * collection.length); //to display a random artwork on button press, we get a random number multiplying by length of the collection so we don't go out of bounds.
+      let piece = collection[i]; //the info about the artwork is stored in a variable
+
+      var artImg = //image is fetched from API, inserting the required id for that art piece into the url. **check if it can be done with template literal instead this looks ugly
+        "https://www.artic.edu/iiif/2/" +
+        piece.image_id +
+        "/full/843,/0/default.jpg";
+
+      var artwork = document.querySelector("#artwork"); //get element where image of the artwork will go
+      artwork.setAttribute("src", artImg); //change the image src attribute to hold the artwork
+
+      var title = document.querySelector("#title"); //get element where title of artwork will go
+      title.innerText = piece.title; //insert text containing title gotten from API
+
+      var artist = document.querySelector("#artist"); //get element where artist's name will go
+      artist.innerText = piece.artist_title; //insert text
+    }
+
+    btn.addEventListener("click", refresh); //on button click, run func refresh and display a new artwork + appropriate title/name
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
